b476d7e42133a20649b4d37e520b835d96a80b4b
Brian <brian.p.ruscio@gmail.com>
2023-08-09 21:01:10 -0400

    Skills Set-up Completed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-import { NavBar, Navbar } from "./componets/NavBar";
+import { NavBar } from "./componets/NavBar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Banner } from './componets/Banner';
+import { Skills } from './componets/Skills';
 
 function App() {
   return (
     <div className='App'>
       <NavBar />
       <Banner />
+      <Skills />
     </div>
   );
 }
 
 export default App;
-
-
-{/* <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          CrackHack <code>IN THE HOUSE</code> with buttfuck
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn how to code
-        </a>
-      </header>
-    </div> */}
---
+++ src/componets/Skills.js
@@ -0,0 +1,98 @@
+import { Container, Row, Col } from "react-bootstrap";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import colorSharp from "../assests/img/color-sharp.png";
+import caro1 from "../assests/img/caro1.png";
+import caro2 from "../assests/img/caro2.png";
+import caro3 from "../assests/img/caro3.svg";
+import caro4 from "../assests/img/caro4.svg";
+import caro5 from "../assests/img/caro5.png";
+import caro6 from "../assests/img/caro6.svg";
+import caro7 from "../assests/img/caro7.png";
+import caro8 from "../assests/img/caro8.svg";
+import caro9 from "../assests/img/caro9.svg";
+import caro10 from "../assests/img/caro10.png";
+
+export const Skills = () => {
+    const responsive = {
+        superLargeDesktop: {
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+
+    return (
+        <section className="skill" id="skills">
+            <Container>
+                <Row>
+                    <Col>
+                        <div className="skill-bx">
+                            <h2>
+                                Skills
+                            </h2>
+                            <p>
+                                These are the frameworks and coding languages I have experience in.
+                            </p>
+                            <Carousel responsive={responsive} infinite={true} className="skill-slider">
+                                <div className="item">
+                                    <img src={caro1} alt="Image" />
+                                    <h5>Python</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={caro2} alt="Image" />
+                                    <h5>Javascript</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={caro3} alt="Image" />
+                                    <h5>HTML</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={caro4} alt="Image" />
+                                    <h5>CSS</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={caro5} alt="Image" />
+                                    <h5>React</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={caro6} alt="Image" />
+                                    <h5>Django</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={caro7} alt="Image" />
+                                    <h5>SQL</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={caro8} alt="Image" />
+                                    <h5>FastAPI</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={caro9} alt="Image" />
+                                    <h5>Git</h5>
+                                </div>
+                                <div className="item">
+                                    <img src={caro10} alt="Image" />
+                                    <h5>Docker</h5>
+                                </div>
+                            </Carousel>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            <img className="background-image-left" src={colorSharp} />
+        </section>
+    );
+
+}
